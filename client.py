b7ef0b4103ccc614d6891c4ce582dedf8ffae67d
Matthias <matt.mll@hotmail.com>
2022-03-20 15:35:30 +0100

    updated header and chunked retrieving fixed

--- client.py
+++ client.py
@@ -1,15 +1,10 @@
 from ast import Str
 import sys, socket
 
-from attr import define
-
-# GET 	: html body + metainfo
-# HEAD 	: metainfo
-# PUT 	: ?
-# POST 	: ?
-
 class ClientSocket:
-	BUFFERSIZE = 4096
+	BUFFERSIZE = 1
+	end_of_header = "\r\n\r\n"
+	stop = "\r\n"
 
 	command = ""
 	uri = ""
@@ -32,7 +27,9 @@ class ClientSocket:
 	def create_socket(self):
 		try:
 			self.ip = socket.gethostbyname(self.uri)
-			self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# AF_INET = ipv4; SOCK_STREAM = standaard protocol
+			self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#	AF_INET = ipv4; SOCK_STREAM = TCP
+			self.soc.setblocking(0)		#	Voor select() later, select.select([socket], [], [], 5); imort select
+			self.soc.settimeout(5)		#	Eerst dit testen 
 		except socket.error as e:
 			print(e)
 
@@ -57,9 +54,6 @@ class ClientSocket:
 			self.post(command, input(), length)
 		elif command == "PUT":
 			self.put(command, input() , length)
-
-	def retrieve_embedded_images(self):
-		pass
 
 	def check_charset(self, header):
 		substr = "charset="
@@ -117,82 +111,65 @@ class ClientSocket:
 
 			return int(s)
 
-	def make_html_output(self):
-		start_html = self.response.find("<!doctype html>")	#	Not always in html file
+	def retrieve_embedded_images(self):
+		pass
 
-		if start_html == -1:
-			start_html = self.response.find("<html>")		#	Always in html file, not plaintext
+	def get_header(self):
+		buffer = self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
+		
+		while self.end_of_header not in buffer:
+			buffer += self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
 
-		if start_html == -1:
-			substr = "\r\n\r\n"
+		return buffer
 
-			start_html = self.response.find(substr)		# 	Then it is plaintext
+	def get_chunked(self):
+		def in_buffer(buffer):
+			if len(buffer) > len(self.stop):
+				if buffer[-len(self.stop):] == self.stop:
+					return False
 
-			self.response = self.response[start_html + len(substr):]
-		else:
-			self.response = self.response[start_html:]
+			return True
 
-	def remove_end(self):
-		if self.filetype == "html":
-			if "<HTML>" in self.response:
-				substr = "</HTML>"
-				end = self.response.find(substr)
-				self.response = self.response[:(end+len(substr))]
-			else:
-				substr = "</html>"
-				end = self.response.find(substr)
-				self.response = self.response[:(end+len(substr))]
+		def get_chunksize():
+			buffer = ""
+			while in_buffer(buffer):
+				buffer += self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
 
-	def get_chunked(self, buffer):
-		if self.filetype == "html":
-			if "<HTML>" in buffer:
-				while "</HTML>" not in buffer:
-					self.response += buffer
-					buffer = self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
+			if buffer[:len(self.stop)] == self.stop:
+				buffer = buffer[len(self.stop):]
+				
+			return int(buffer[:-len(self.stop)], base=16)
 
-				self.response += buffer
-			else:
-				while "</html>" not in buffer:
-					self.response += buffer
-					buffer = self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
+		size = get_chunksize()
+		while size != 0:
+			self.response += self.soc.recv(size).decode(encoding=self.charset)
+			size = get_chunksize()
 
-				self.response += buffer
-		else:
-			self.response += buffer	# TODO: Hoe het einde dan weten? Is dit zelfs nodig?
-
-	def get_whole(self, buffer, chunk):
-		self.response += buffer
-
-		if len(buffer) < chunk:
-			buffer = self.soc.recv(chunk - self.BUFFERSIZE).decode(encoding=self.charset)
-			self.response += buffer
+	def get_whole(self, chunk):
+		self.response += self.soc.recv(chunk).decode(encoding=self.charset)
 
 	def get(self, command):
 		self.request = command + " / HTTP/1.1\r\nHost: " + self.uri + "\r\n\r\n"
 		self.soc.send(self.request.encode())
 		
 		#	This call receives the header
-		buffer = self.soc.recv(self.BUFFERSIZE).decode(encoding=self.charset)
-
-		self.check_charset(buffer)
-		chunk = self.check_page_length(buffer)		#	If chunked: chunk = -1
-		filetype_1, self.filetype = self.check_file_type(buffer)
+		header = self.get_header()
+		self.check_charset(header)
+		chunk = self.check_page_length(header)		#	If chunked: chunk = -1
+		filetype_1, self.filetype = self.check_file_type(header)
 
 		if chunk == -1:
-			self.get_chunked(buffer)
+			self.get_chunked()
 		else:
-			self.get_whole(buffer, chunk)
+			self.get_whole(chunk)
 
 		self.retrieve_embedded_images()
-
-		self.make_html_output()
-		self.remove_end()
 
 	def head(self, command):
 		self.request = command + " / HTTP/1.1\r\nHost: " + self.uri + "\r\n\r\n"
 		self.soc.send(self.request.encode())
 
-		self.response = self.soc.recv(4096).decode(encoding=self.charset)		# Mogelijks niet lang genoeg
+		self.response = self.get_header()
 
 	def post(self, command, path, length):
 		self.request = command + " " + path + " HTTP/1.1\r\nHost: " + self.uri + "\r\n" + "Content-Length: " + str(length) + "\r\n\r\n"
