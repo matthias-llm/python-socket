d98db810aded999e922e2f03e5f57a425378de17
Janpieter <janpieter.manteleers@student.kuleuven.be>
2022-04-17 00:03:04 +0200

    Tot POST, werkende

--- server/server.py
+++ server/server.py
@@ -1,16 +1,18 @@
+
 from builtins import print
+import os
 import time
 import socket
 import threading
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5582
+PORT = 5594
 ADDR = (IP, PORT)
 SIZE = 1
 FORMAT = "utf-8"
 DISCONNECT_MSG = "DISCONNECT!"
 STATUS_CODE = {200: "200 OK", 404: "404 Not Found",
-               400: "Bad Request", 500: "Server Error", 304: "Not Modified"}
+               400: "400 Bad Request", 500: "500 Server Error", 304: "304 Not Modified"}
 TIME = time.strftime("%a, %d %b %Y %I:%M:%S %Z", time.gmtime())
 
 
@@ -33,12 +35,10 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         header = get_PART(conn, SIZE)
-        print(header)
         if DISCONNECT_MSG == header.rstrip():
             connected = False
         if connected:
             parts = split_HEADER(header)
-            print("parts", parts)
             if check_HOST(parts, conn):
                 if parts[0] == "PUT" or parts[0] == "POST":
                     length_next_chunk = int(parts[3]['Content length'])
@@ -67,6 +67,39 @@ def redirect_msg(parts, conn):
         headers = f"DATE: {TIME}\r\nContent type: plain/text\r\nContent length: {c_length}"
         respond(conn, stat_line, headers, response_body)
         return
+    return
+
+
+def handle_PUT(parts, conn):
+    path = os.path.dirname(os.path.abspath(__file__))
+    file_name = parts[1][1:]
+    file_name = file_name.split('.', 2)[0]+".txt"
+    with open(os.path.join(path, file_name), 'w') as newfile:
+        newfile.write(parts[4])
+    status_line = "HTTP/1.1 " + STATUS_CODE[200] + "\r\n"
+    response_body = "<html><body><h1>The file was created.</h1></body></html>"
+    c_length = len(response_body.encode(FORMAT))
+    headers = f"DATE: {TIME}\r\nContent type: plain/text\r\nContent length: {c_length}"
+    respond(conn, status_line, headers, response_body)
+    return
+
+
+def handle_POST(parts, conn):
+    file_name = parts[1][1:]
+    file_name = file_name.split('.', 2)[0]+".txt"
+    path = os.path.dirname(os.path.abspath(__file__))
+    dir_list = os.listdir(path)
+    if file_name not in dir_list:
+        handle_PUT(parts, conn)
+    else:
+        with open(os.path.join(path, file_name), 'a') as newfile:
+            newfile.write(parts[4])
+        status_line = "HTTP/1.1 " + STATUS_CODE[200] + "\r\n"
+        response_body = "<html><body><h1>the content was succesfully appended.</h1></body></html>"
+        c_length = len(response_body.encode(FORMAT))
+        headers = f"DATE: {TIME}\r\nContent type: plain/text\r\nContent length: {c_length}"
+        respond(conn, status_line, headers, response_body)
+    return
 
 
 def handle_GET(parts, conn):
@@ -74,14 +107,6 @@ def handle_GET(parts, conn):
 
 
 def handle_HEAD(parts, conn):
-    pass
-
-
-def handle_POST(parts, conn):
-    pass
-
-
-def handle_PUT(parts, conn):
     pass
 
 
@@ -114,7 +139,6 @@ def split_HEADER(msg):
     # hier moet nog een big check komen
     request = ''.join((line + '\n') for line in msg.splitlines())
     request_head = request.splitlines()  # List of request and headers
-    print("request_head", request_head)
     request_line = request_head[0]
     method, uri, http = request_line.split(' ', 3)
     headers = {}
@@ -126,37 +150,6 @@ def split_HEADER(msg):
 
 if __name__ == "__main__":
     main()
-
-
-# def handle_msg(msg: str, conn):
-#     request = normalize_msg(msg)
-#     try:
-#         request_head, body = request.split('\n\n', 1)
-#         print("request head:", request_head, "body", body)
-#     except:
-#         status_line: str = STATUS_CODE[400]
-#         response_body = "<html><body><h1>No valid method</h1></body></html>"
-#         header = "Date: " + TIME + "\r\n" + "Content type: " + "plain/text" + \
-#             + "\r\n" + "Content length: "  # + \
-#         # str(len(response_body.encode(FORMAT)))
-#         send_RESPONSE(conn, status_line, header, response_body)
-#     request_head = request_head.splitlines()  # List of request and headers
-#     request_line = request_head[0]
-#     method, uri, http = request_line.split(' ', 3)
-#     headers = {}
-#     for head in request_head[1:]:
-#         elem = head.split(": ", 1)
-#         headers[elem[0]] = elem[1]
-#         print(method, uri, http, headers, "body", body)
-#     return [method, uri, http, headers, body]
-
-
-# def handle_GET(msg, conn):
-#     check_HOST_HEADER(msg, conn)
-
-
-# def handle_HEAD(msg, conn):
-#     check_HOST_HEADER(msg, conn)
 
 
 # def handle_PUT(msg, conn):
